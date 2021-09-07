d9c67d0fc65b79b23731d35782b1206e2835a450
lsq <359549701@qq.com>
2021-09-07 22:05:20 +0800

    markdown解析

--- pages/Detailed.js
+++ pages/Detailed.js
@@ -13,42 +13,86 @@ import Author from '../component/Author'
 import Advert from '../component/Advert';
 import Footer from '../component/Footer';
 
-const Detailed = () => (
-    <div>
-        <Head>
-            <title>Detailed</title>
-        </Head>
-        <Header></Header>
-        <Row className="comm-main" type='flex' justify='center'>
-            <Col className='comm-left' xs={24} sm={24} md={16} lg={18} xl={14}>
-                <div className={styles.breadDiv}>
-                    <Breadcrumb>
-                        <Breadcrumb.Item><a href='/'>首页</a></Breadcrumb.Item>
-                        <Breadcrumb.Item>视频列表</Breadcrumb.Item>
-                        <Breadcrumb.Item>xxxx</Breadcrumb.Item>
-                    </Breadcrumb>
-                </div>
-                <div>
-                    <div className={styles.detailedTitle}>
-                        React项目实战开发
-                    </div>
-                    <div className={styles.center + ' list-icon'}>
-                        <span><CalendarOutlined />2021/9/7</span>
-                        <span><FolderOutlined />&nbsp;视频教程</span>
-                        <span><FireOutlined />&nbsp;2222人</span>
-                    </div>
-                    <div className={styles.detailedContent}>
-                        mardoen内容
-                    </div>
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm'
 
-                </div>
-            </Col>
-            <Col className='comm-right' xs={0} sm={0} md={7} lg={5} xl={4}>
-                <Author />
-                <Advert />
-            </Col>
-        </Row>
-        <Footer />
-    </div>
-)
+const Detailed = () => {
+    let markdown = '# P01:课程介绍和环境搭建\n' +
+        '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
+        '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
+        '**这是加粗的文字**\n\n' +
+        '*这是倾斜的文字*`\n\n' +
+        '***这是斜体加粗的文字***\n\n' +
+        '~~这是加删除线的文字~~ \n\n' +
+        ' ``` console.log(111) ``` \n\n' +
+        '# p02:来个Hello World 初始Vue3.0\n' +
+        '> aaaaaaaaa\n' +
+        '>> bbbbbbbbb\n' +
+        '>>> cccccccccc\n' +
+        '***\n\n\n' +
+        '# p03:Vue3.0基础知识讲解\n' +
+        '> aaaaaaaaa\n' +
+        '>> bbbbbbbbb\n' +
+        '>>> cccccccccc\n\n' +
+        '# p04:Vue3.0基础知识讲解\n' +
+        '> aaaaaaaaa\n' +
+        '>> bbbbbbbbb\n' +
+        '>>> cccccccccc\n\n' +
+        '#5 p05:Vue3.0基础知识讲解\n' +
+        '> aaaaaaaaa\n' +
+        '>> bbbbbbbbb\n' +
+        '>>> cccccccccc\n\n' +
+        '# p06:Vue3.0基础知识讲解\n' +
+        '> aaaaaaaaa\n' +
+        '>> bbbbbbbbb\n' +
+        '>>> cccccccccc\n\n' +
+        '# p07:Vue3.0基础知识讲解\n' +
+        '> aaaaaaaaa\n' +
+        '>> bbbbbbbbb\n' +
+        '>>> cccccccccc\n\n' +
+        '``` var a=11; ```'
+
+    return (
+        <div>
+            <Head>
+                <title>Detailed</title>
+            </Head>
+            <Header></Header>
+            <Row className="comm-main" type='flex' justify='center'>
+                <Col className='comm-left' xs={24} sm={24} md={16} lg={18} xl={14}>
+                    <div className={styles.breadDiv}>
+                        <Breadcrumb>
+                            <Breadcrumb.Item><a href='/'>首页</a></Breadcrumb.Item>
+                            <Breadcrumb.Item>视频列表</Breadcrumb.Item>
+                            <Breadcrumb.Item>xxxx</Breadcrumb.Item>
+                        </Breadcrumb>
+                    </div>
+                    <div>
+                        <div className={styles.detailedTitle}>
+                            React项目实战开发
+                        </div>
+                        <div className={styles.center + ' list-icon'}>
+                            <span><CalendarOutlined />2021/9/7</span>
+                            <span><FolderOutlined />&nbsp;视频教程</span>
+                            <span><FireOutlined />&nbsp;2222人</span>
+                        </div>
+                        <div className={styles.detailedContent}>
+                            <ReactMarkdown
+                                children={markdown}
+                                skipHtml={false}
+                                remarkPlugins={[remarkGfm]}
+                            />
+                        </div>
+
+                    </div>
+                </Col>
+                <Col className='comm-right' xs={0} sm={0} md={7} lg={5} xl={4}>
+                    <Author />
+                    <Advert />
+                </Col>
+            </Row>
+            <Footer />
+        </div>
+    )
+}
 export default Detailed
